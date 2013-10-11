790db429de5f61a01b72559292fa1819adf02747
Andreas Waelchli <andreas.waelchli@me.com>
2013-10-11 20:58:14 +0200

    Added Abstract Dictionary ADictionary in core

---
+++ WordFinderCore/src/core/ADictionary.java
@@ -0,0 +1,15 @@
+package core;
+
+public abstract class ADictionary implements DictionaryInterface {
+
+	
+	
+	private ArrayList<String> words;
+	
+	@Override
+	public boolean containsWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
