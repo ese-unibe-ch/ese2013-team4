2bc4a457a8d9e42400f7c8bce236d6f39a8ed48e
Andreas Waelchli <andreas.waelchli@me.com>
2013-10-11 17:01:26 +0200

    edited gitignore

--- WordFinderCore/src/core/BoardInterface.java
+++ WordFinderCore/src/core/BoardInterface.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.ArrayList;
+
 public interface BoardInterface {
 
 	public LetterFieldInterface[][] getMatrix();
@@ -11,5 +13,32 @@ public interface BoardInterface {
 	 * @param y vertical coordinate of the letter (0: top)
 	 */
 	public LetterFieldInterface getSquare(int x, int y);
+
+	public ArrayList<DictionaryInterface> getDictionaries();
 	
+	/**
+	 * Select the Letter at the given coordinate (x|y).
+	 * 
+	 * If there's no letter selected up to now, it creates a new sequence.
+	 * If the Letter already is selected, it is unselected.
+	 * 		All subsequent letters are unselected as well.
+	 * If the Letter is not adjacent to the one selected before,
+	 * 		it drops all previously selected letters and starts a new sequence
+	 * 
+	 * @param x horizontal coordinate of the letter (0: left)
+	 * @param y vertical coordinate of the letter (0: top)
+	 */
+	public void select(int x, int y);
+	
+	/**
+	 * returns a word object constructed out of the currently selected sequence.
+	 * 
+	 * returns an empty word if there's no selection
+	 */
+	public WordInterface getSelectedSequence();
+	
+	/**
+	 * clears the current selection
+	 */
+	public void unselectAll();
 }
