2691e6a2e521e08386678522583ed33153feba83
selinerdominik <selinerdominik@gmail.com>
2013-10-13 14:12:01 +0200

    added JUnit and WordTest
    Added the JUnit library and wrote a test for the class Word

---
+++ WordFinderCore/src/core/WordTest.java
@@ -0,0 +1,56 @@
+package core;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cases for Word Class.
+ * @author $Author: Team4 $
+ */
+
+public class WordTest {
+	private Word word;
+	private ArrayList<Letter> letter_list;
+	int wordcount;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.word = new Word();
+		letter_list = new ArrayList<Letter>();
+		letter_list.add(Letter.A);
+		letter_list.add(Letter.B);
+		letter_list.add(Letter.C);
+		this.wordcount = Letter.A.getValue() + Letter.B.getValue() + Letter.C.getValue();
+	}
+	
+	
+	/**
+	 * Test the getters and setters.
+	 */
+	
+	@Test
+	public void setLetterSequenceTest() {
+		assertTrue(this.word.getLetterSequence().isEmpty());
+		this.word.setLetterSequence(this.letter_list);
+		assertEquals(this.letter_list, this.word.getLetterSequence());
+		
+	}
+	
+	/**
+	 * Test if the value of a word is calculated Correctly.
+	 */
+	
+	@Test
+	public void getWordValueTest() {
+		assertTrue(this.word.getLetterSequence().isEmpty());
+		this.word.setLetterSequence(this.letter_list);
+		assertEquals(this.wordcount, this.word.getWordValue());
+		
+	}
+	
+}
