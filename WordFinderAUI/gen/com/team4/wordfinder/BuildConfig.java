00d977e2e99480de843e5024d5d23c898071cc78
domenico iapello <domenico.iapello@gmail.com>
2013-10-20 09:07:21 +0200

    pull merge conflict solved

---
+++ WordFinderAUI/gen/com/team4/wordfinder/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.team4.wordfinder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
