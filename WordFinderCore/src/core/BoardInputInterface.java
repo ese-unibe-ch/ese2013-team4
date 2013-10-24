e36caf8e6563a422bbf9017b7a7eb7c9b0da2e17
Andreas Waelchli <andreas.waelchli@me.com>
2013-10-24 11:49:03 +0200

    added Enum for representing Selection states after submitting one

---
+++ WordFinderCore/src/core/BoardInputInterface.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Interface with all methods needed to select fields of the board and submit them as solution.
+ * 
+ */
+public interface BoardInputInterface {
+
+	
+	
+}
---
+++ WordFinderCore/src/core/SelectionStatus.java
@@ -0,0 +1,10 @@
+package core;
+
+public enum SelectionStatus {
+
+	SelectionGood, //new valid word. added to the "found words" list
+	SelectionOld, //valid word, but already found. (only if the same word was found at the same place)
+	SelectionBad, //valid sequence, but no word.
+	SelectionInvalid; //invalid sequence. (multiple use of fields)
+	
+}
