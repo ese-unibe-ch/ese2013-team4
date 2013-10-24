0021503a009b380aadac577fe1eb19f196c2950b
Andreas Waelchli <andreas.waelchli@me.com>
2013-10-24 11:57:40 +0200

    Created BoardInputInterface, Point, excluded Core JavaDoc from git

--- WordFinderCore/src/core/BoardInputInterface.java
+++ WordFinderCore/src/core/BoardInputInterface.java
@@ -1,11 +1,21 @@
 package core;
 
+import java.util.ArrayList;
+
 /**
  * Interface with all methods needed to select fields of the board and submit them as solution.
  * 
  */
 public interface BoardInputInterface {
 
+	/**
+	 * Submits a solution given as the sequence of the field selection.
+	 * 
+	 * @param sequence
+	 * @return status of selection
+	 * @see SelectionStatus
+	 */
+	public SelectionStatus submit(ArrayList<Point> sequence);
 	
 	
 }
