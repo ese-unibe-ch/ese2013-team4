ef57e6013100f01c07e9f0dd0a7ceaae74afa629
Timotheos91 <timkun@hotmail.com>
2013-10-29 00:03:40 +0100

    convertet timer to timeable variable

--- WordFinderTwo/gen/com/example/wordfindertwo/R.java
+++ WordFinderTwo/gen/com/example/wordfindertwo/R.java
@@ -19,6 +19,7 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int button_size=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -29,7 +30,7 @@ public final class R {
         public static final int PlayGame=0x7f080002;
         public static final int Score=0x7f080003;
         public static final int action_settings=0x7f080006;
-        public static final int textView1=0x7f080000;
+        public static final int linearLayout1=0x7f080000;
         public static final int textView2=0x7f080001;
     }
     public static final class layout {
--- WordFinderTwo/src/com/example/wordfindertwo/Game.java
+++ WordFinderTwo/src/com/example/wordfindertwo/Game.java
@@ -1,39 +1,68 @@
 package com.example.wordfindertwo;
 
 import com.example.wordfindertwo.R;
-
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.TextView;
 
-public class Game extends Activity {
+public class Game extends Activity implements OnTouchListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-		//countdown atm 2min displayed in sec
-		new CountDownTimer(120000, 1000){
-			
-			//displays a new time every tick
-			public void onTick(long millisUntilFinished){
+
+		// countdown atm 2min displayed in sec
+		CountDownTimer timer = new CountDownTimer(120000, 1000) {
+
+			// displays a new time every tick
+			public void onTick(long millisUntilFinished) {
 				TextView text = (TextView) findViewById(R.id.textView2);
-				text.setText("You have " + (millisUntilFinished/1000)+ " sec remaining!");
+				text.setText("You have " + (millisUntilFinished / 1000)
+						+ " sec remaining!");
 			}
-			
-			public void onFinish(){
+
+			public void onFinish() {
 				TextView text = (TextView) findViewById(R.id.textView2);
 				text.setText("Time's up!");
+
 			}
-		}.start();
+		};
+		timer.start();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.game, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onTouch(View arg0, MotionEvent me) {
+		// TODO Auto-generated method stub
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		switch (me.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+
+			break;
+		case MotionEvent.ACTION_UP:
+
+			break;
+		case MotionEvent.ACTION_MOVE:
+
+			break;
+		}
 		return true;
 	}
 
