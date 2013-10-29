dffe27a84c78c0334f03da0495aa339cb373f8f7
Andreas Waelchli <andreas.waelchli@me.com>
2013-10-29 12:26:05 +0100

    WORD FINDING IS BUGGED
    ----> also breaks board generation

--- WordFinderCore/src/core/test/BoardFactoryTest.java
+++ WordFinderCore/src/core/test/BoardFactoryTest.java
@@ -4,8 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import core.BoardFactory;
+import core.board.Board;
+
 public class BoardFactoryTest {
 
-	
+	public static void main(String[] args) {
+		Board b = BoardFactory.createDemoBoard();
+		for (int y = 0; y < b.getBoardSize(); y++) {
+			for (int x = 0; x < b.getBoardSize(); x++) {
+				System.out.print(b.getLetterAt(x, y).getChar());
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (String w : b.getWordsInBoard()) {
+			System.out.println(w);
+		}
+	}
 
 }
