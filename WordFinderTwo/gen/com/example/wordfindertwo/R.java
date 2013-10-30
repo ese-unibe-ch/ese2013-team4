71ac7eb3b26221f2998e8392690f6beafb68ac3c
Timotheos91 <timkun@hotmail.com>
2013-10-30 12:27:11 +0100

    in "game activity" added start and pause button to start and pause the timer

--- WordFinderTwo/gen/com/example/wordfindertwo/R.java
+++ WordFinderTwo/gen/com/example/wordfindertwo/R.java
@@ -25,26 +25,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Dictionary=0x7f080009;
-        public static final int Friend=0x7f080008;
-        public static final int PlayGame=0x7f080006;
-        public static final int Score=0x7f080007;
-        public static final int action_settings=0x7f08000b;
+        public static final int Dictionary=0x7f08000c;
+        public static final int Friend=0x7f08000b;
+        public static final int PlayGame=0x7f080009;
+        public static final int Score=0x7f08000a;
+        public static final int action_settings=0x7f08000e;
         public static final int button1=0x7f080001;
         public static final int deleteFriend=0x7f080002;
-        public static final int friendListRawLayout=0x7f08000a;
+        public static final int friendListRawLayout=0x7f08000d;
         public static final int friendsListView=0x7f080003;
         public static final int linearLayout1=0x7f080004;
+        public static final int pausetimer=0x7f080007;
+        public static final int starttimer=0x7f080006;
         public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080005;
+        public static final int textView2=0x7f080008;
+        public static final int timer=0x7f080005;
     }
     public static final class layout {
         public static final int activity_dictionary=0x7f030000;
         public static final int activity_friendtable=0x7f030001;
         public static final int activity_game=0x7f030002;
         public static final int activity_main_menu=0x7f030003;
-        public static final int activity_score=0x7f030004;
-        public static final int friend_list_row_layout=0x7f030005;
+        public static final int activity_pause_screen=0x7f030004;
+        public static final int activity_score=0x7f030005;
+        public static final int friend_list_row_layout=0x7f030006;
     }
     public static final class menu {
         public static final int dictionary=0x7f070000;
@@ -52,7 +56,8 @@ public final class R {
         public static final int game=0x7f070002;
         public static final int main=0x7f070003;
         public static final int main_menu=0x7f070004;
-        public static final int score=0x7f070005;
+        public static final int pause_screen=0x7f070005;
+        public static final int score=0x7f070006;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -70,6 +75,7 @@ public final class R {
         public static final int title_activity_friend=0x7f050005;
         public static final int title_activity_game=0x7f050006;
         public static final int title_activity_main=0x7f050003;
+        public static final int title_activity_pause_screen=0x7f050010;
         public static final int title_activity_score=0x7f050004;
     }
     public static final class style {
