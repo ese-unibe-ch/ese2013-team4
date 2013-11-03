913d9f3a6a9216484ddfdfc23e76977340942971
selinerdominik <selinerdominik@gmail.com>
2013-11-03 14:49:14 +0100

    Added The TestingProject and Ants stuff

---
+++ WordFinderTestTest/bin/MainMenuTest-debug-unaligned.apk.d
@@ -0,0 +1,3 @@
+/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/MainMenuTest-debug-unaligned.apk : \
+/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/MainMenuTest.ap_ \
+/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/classes.dex \
