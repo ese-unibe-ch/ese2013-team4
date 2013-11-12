170afd80d2fd40469e2f8e192c03d9bf924bb624
domenico iapello <domenico.iapello@gmail.com>
2013-11-12 12:15:29 +0100

    friendsList to table and some changes

--- WordFinderTwo/src/com/example/wordfindertwo/FriendsTable.java
+++ WordFinderTwo/src/com/example/wordfindertwo/FriendsTable.java
@@ -1,40 +1,82 @@
 package com.example.wordfindertwo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import android.os.Bundle;
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 
-public class FriendsTable extends ListActivity {
+public class FriendsTable extends Activity{
 
 
-	RadioButton deleteOn;
-	Button addFriend;
+	RadioButton deleteOnRadioButton;
+	Button addFriendButton;
 	
-	ListView friendsList;
+	private TableLayout friendsTable_scrollView_TableLayout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_friendtable);
 		
-		deleteOn = (RadioButton) findViewById(R.id.deleteFriend);
-		addFriend = (Button) findViewById(R.id.addFriendButton);
+		friendsTable_scrollView_TableLayout =(TableLayout) findViewById(R.id.friendsTable_scrollView_TableLayout);
+		deleteOnRadioButton = (RadioButton) findViewById(R.id.deleteFriend);
+		addFriendButton = (Button) findViewById(R.id.addFriendButton);
 		
-		
-		friendsList = (ListView) findViewById(R.id.friendsListListView);
-		
+		deleteOnRadioButton.setOnClickListener((android.view.View.OnClickListener) deleteOnRadioButtonListener);
+				
 	}
+	
+	private OnClickListener deleteOnRadioButtonListener =new OnClickListener() {
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			View button = findViewById(R.id.friends_tableRow_deleteButton);
+			button.setVisibility(View.VISIBLE);
+			
+		}
+	};
+	
+	private void insertFriendInScrollView(String friendNameMail, int Arrayindex){
+		
+		
+		LayoutInflater friendsInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View newFriendRow = friendsInflater.inflate(R.layout.activity_friend_tablerow, null);
+		TextView friends_tablRow_TextsView = (TextView) newFriendRow.findViewById(R.id.friends_tablRow_TextsView);
+		friends_tablRow_TextsView.setText(friendNameMail);
+		
+		Button friends_tableRow_deleteButton = (Button) newFriendRow.findViewById(R.id.friends_tableRow_deleteButton);
+		friends_tableRow_deleteButton.setOnClickListener((android.view.View.OnClickListener) friends_tableRow_deleteButtonListener);
+
+		friendsTable_scrollView_TableLayout.addView(friends_tablRow_TextsView, Arrayindex);
+	}
+	
+	
+	
+	private OnClickListener friends_tableRow_deleteButtonListener= new OnClickListener() {
+		
+		
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			friendsTable_scrollView_TableLayout.removeViewAt(which);
+			
+			
+		}
+	};
+	
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,9 +91,5 @@ public class FriendsTable extends ListActivity {
 		Intent intent = new Intent(this, AddFriend.class);
 		startActivity(intent);
 	}
-
-	public ArrayList<String> getFriendList(){
-		return null;
 		
-	}
 }
