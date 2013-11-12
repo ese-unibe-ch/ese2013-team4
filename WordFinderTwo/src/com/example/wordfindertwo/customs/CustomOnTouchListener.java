785dbdde12e5fc0afd2544490de985dfb2e107c7
Timotheos91 <timkun@hotmail.com>
2013-11-13 00:38:40 +0100

    problem with not showing if the chosen word is correct still exists
    somehow have to tell the UI to update more fequently

--- WordFinderTwo/src/com/example/wordfindertwo/customs/CustomOnTouchListener.java
+++ WordFinderTwo/src/com/example/wordfindertwo/customs/CustomOnTouchListener.java
@@ -36,114 +36,91 @@ public class CustomOnTouchListener implements OnTouchListener, Runnable {
 	public boolean onTouch(View view, MotionEvent me) {
 		int event = me.getAction();
 
-		if (event == MotionEvent.ACTION_DOWN || event == MotionEvent.ACTION_MOVE || event == MotionEvent.ACTION_UP) {
-
-			CustomButton button = ButtonListProvider.getInstance().getButtonUnder(me.getRawX(), me.getRawY());
-			
-			if (button != null && (buttonList.size() == 0 || !buttonList.get(buttonList.size() - 1).getPoint().equals(button.getPoint()))) {
+		if (event == MotionEvent.ACTION_DOWN
+				|| event == MotionEvent.ACTION_MOVE
+				|| event == MotionEvent.ACTION_UP) {
+			CustomButton button = ButtonListProvider.getInstance()
+					.getButtonUnder(me.getRawX(), me.getRawY());
+			if (button != null
+					&& (buttonList.size() == 0 || !buttonList
+							.get(buttonList.size() - 1).getPoint()
+							.equals(button.getPoint()))) {
 				buttonList.add(button);
 				// TODO: color new button
 				button.setBackgroundColor(Color.CYAN);
-				
-				Log.d("CustomOnTouchListener", "moved to " + button.getPoint().toString() + " - List Size is now " + this.buttonList.size());
+				Log.d("CustomOnTouchListener", "moved to "
+						+ button.getPoint().toString() + " - List Size is now "
+						+ this.buttonList.size());
 			}
-
 		}
-		
+
 		if (event == MotionEvent.ACTION_UP) {
-			
 			Log.i("CustomOnTouchListener", "finishing sequence");
-			
 			ArrayList<Point> pointList = new ArrayList<Point>();
-			
 			for (CustomButton btn : buttonList) {
 				pointList.add(btn.getPoint());
 			}
-			
 
 			// TODO: add
-			
+
 			result = board.submit(pointList);
-			
-			//EVALUATE RESULT
-			colorThread = new Thread(this);
-			colorThread.run();
-			
-			
-			//PRINT SCORE
+			colorThread = new Thread();
+
+			// EVALUATE RESULT
+			switch (result) {
+			case SelectionGood:
+				// TODO: color green
+				for (CustomButton btn : buttonList) {
+					btn.setBackgroundColor(Color.GREEN);
+				}
+				game.update();
+				break;
+
+			case SelectionOld:
+				// TODO: color yellow
+				for (CustomButton btn : buttonList) {
+					btn.setBackgroundColor(Color.YELLOW);
+				}
+				break;
+
+			case SelectionBad:
+				// TODO: color red
+				for (CustomButton btn : buttonList) {
+					btn.setBackgroundColor(Color.RED);
+				}
+				break;
+			case SelectionInvalid:
+				break;
+			}
+
+			// PRINT SCORE
 			score = (TextView) game.findViewById(R.id.score);
 			score.setText("" + board.getBoardScore());
-			// TODO: delay (short)
-			// TODO: paint neutral
-			//cleanup
+
+			// delay (short)
+			colorThread.run();
+
+			// paint neutral
+			for (CustomButton btn : ButtonListProvider.getInstance().getList()) {
+				btn.setBackgroundResource(android.R.drawable.btn_default);
+			}
+			// cleanup
 			buttonList.clear();
 		}
+
 		// END
 		return true;
 	}
 
-	public void motionEventDown(CustomButton v) {
-
-	}
-
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		switch (result) {
-		case SelectionGood:
-			// TODO: color green
-			for (CustomButton btn : buttonList){
-				btn.setBackgroundColor(Color.GREEN);
-			}
-			// one sec pause
-			try {
-				colorThread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			for (CustomButton btn : buttonList){
-				btn.setBackgroundResource(android.R.drawable.btn_default);
-			}
+		try {
+			colorThread.join();
+			colorThread.sleep(1000);
 			colorThread.interrupt();
-			game.update();
-			break;
-			
-		case SelectionOld:
-			// TODO: color yellow
-			for (CustomButton btn : buttonList){
-				btn.setBackgroundColor(Color.YELLOW);
-			}
-			// one sec pause
-			try {
-				colorThread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			for (CustomButton btn : buttonList){
-				btn.setBackgroundResource(android.R.drawable.btn_default);			}
-			colorThread.interrupt();
-			break;
-			
-		case SelectionBad:
-			// TODO: color red
-			for (CustomButton btn : buttonList){
-				btn.setBackgroundColor(Color.RED);
-			}
-			// one sec pause
-			try {
-				colorThread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			for (CustomButton btn : buttonList){
-				btn.setBackgroundResource(android.R.drawable.btn_default);			}
-			colorThread.interrupt();
-			break;
-		case SelectionInvalid:
-			break;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
