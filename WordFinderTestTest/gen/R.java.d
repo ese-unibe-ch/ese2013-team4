03f63e20a3548cc84eaf30ee74055acdd73ce661
Andreas Wälchli <andreas.waelchli@me.com>
2013-11-21 00:10:58 +0100

    keine Ahnung...

--- WordFinderTestTest/gen/R.java.d
+++
@@ -1,7 +0,0 @@
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/gen/com/example/wordfindertwo/test/R.java \
- : /Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/res/drawable-hdpi/ic_launcher.png \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/res/drawable-ldpi/ic_launcher.png \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/res/drawable-mdpi/ic_launcher.png \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/res/drawable-xhdpi/ic_launcher.png \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/res/values/strings.xml \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/AndroidManifest.xml \
