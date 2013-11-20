c5999cd3ee9dce5ffdb0baff619ba87b069c3675
Andreas Wälchli <andreas.waelchli@me.com>
2013-11-21 00:02:41 +0100

    gitignore for test project

--- WordFinderTestTest/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/AndroidManifest.xml : \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/AndroidManifest.xml \
--- WordFinderTestTest/bin/MainMenuTest-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/MainMenuTest-debug-unaligned.apk : \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/MainMenuTest.ap_ \
-/Users/dominikseliner/Documents/ese2013-team4/WordFinderTestTest/bin/classes.dex \
